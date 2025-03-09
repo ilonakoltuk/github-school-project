2753596c1d8f64c34ed16d00432b0cf6385bafa1
ilonakoltuk <ilonakoltuk@gmail.com>
2025-03-09 12:55:06 +0000

    Add 4.cpp

---
+++ src/4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  if (x > 3) {
+    cout << "x is greater than 3" << endl;
+  } else {
+    cout << "x is not greater than 3" << endl;
+  }
+  return 0;
+}
