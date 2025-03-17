cd9bb81c1af253a0565958aee0a995e6de119c5a
ilonakoltuk <ilonakoltuk@gmail.com>
2025-03-17 06:08:04 +0000

    Add 12.cpp

---
+++ src/12.cpp
@@ -0,0 +1,15 @@
+// This is a simple program that demonstrates how to add and subtract numbers using C++.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    // Addition
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << (num1 + num2) << endl;
+
+    // Subtraction
+    cout << "The difference between " << num1 << " and " << num2 << " is: " << (num1 - num2) << endl;
+}
