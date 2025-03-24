10ee07b2f4c8022ebeacad6916aca6e825698a17
ilonakoltuk <ilonakoltuk@gmail.com>
2025-03-24 11:27:37 +0000

    Add 19.cpp

---
+++ src/19.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    if (age >= 18) {
+        cout << "You are old enough to attend school." << endl;
+    } else {
+        cout << "Sorry, you are not old enough to attend school." << endl;
+    }
+    return 0;
+}
